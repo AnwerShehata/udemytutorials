c5d97e40ef2187ea7b3a5c95264dbc9fb400d061
Anwar Shehata <anwershehata@gmail.com>
2018-11-05 13:20:50 +0300

    نهاية المستوي الثاني

--- lib/Products.dart
+++ lib/Products.dart
@@ -1,26 +1,28 @@
 import 'package:flutter/material.dart';
 
 class Products extends StatelessWidget {
+  final List<String> products;
 
-final List<String>products;
-  Products(this.products);
+  Products([this.products = const []]) {
+    print('[Products Widget] Constructor');
+  }
 
   @override
   Widget build(BuildContext context) {
-
+    print('[Products Widget] build()');
     return Column(
       children: products
           .map(
             (element) => Card(
-          child: Column(
-            children: <Widget>[
-              Image.network("https://images.pexels.com/photos/862517/pexels-photo-862517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350",fit: BoxFit.cover,),
-              Text(element)
-            ],
-          ),
-        ),
-      ).toList(),
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset('assets/food.jpg'),
+                      Text(element)
+                    ],
+                  ),
+                ),
+          )
+          .toList(),
     );
-
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,19 @@
 import 'package:flutter/material.dart';
-import './ProductManager.dart';
 
+import './product_manager.dart';
 
+void main() => runApp(MyApp());
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget{
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home:  new Scaffold(
-
-        //----------AppBar---------------------
-        appBar: new AppBar(
-          centerTitle: true,
-          elevation: 0.0,
-          title: Text("App"),),
-
-        
-        body: ProductManager("anwer shehaya"),
-
-
-
-
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: ProductManager(startingProduct: 'Food Tester'),
       ),
     );
   }
-
 }

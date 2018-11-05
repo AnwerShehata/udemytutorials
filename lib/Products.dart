3ea4f7ab253c22e50df00300f235acc65ae0af9c
Anwar Shehata <anwershehata@gmail.com>
2018-11-06 01:20:24 +0300

    نهاية المستوي الثالث

--- lib/Products.dart
+++ lib/Products.dart
@@ -10,13 +10,13 @@ class Products extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print('[Products Widget] build()');
-    return Column(
+    return ListView(
       children: products
           .map(
             (element) => Card(
                   child: Column(
                     children: <Widget>[
-                      Image.asset('assets/food.jpg'),
+                      Image.network("https://cdn.pixabay.com/photo/2018/10/22/11/58/grass-3765172__480.jpg",fit: BoxFit.cover,),
                       Text(element)
                     ],
                   ),
--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 
 import './product_manager.dart';
 
-void main() => runApp(MyApp());
+void main()  {
+  runApp(MyApp());}
+
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text('EasyList'),

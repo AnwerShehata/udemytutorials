925cf251fde8e5de3232062b035a6cda7798f2e7
Anwar Shehata <anwershehata@gmail.com>
2018-11-06 20:02:43 +0300

    نهاية المستوي الخامس 5. Working with Lists & Conditionals

--- lib/Products.dart
+++ lib/Products.dart
@@ -6,23 +6,45 @@ class Products extends StatelessWidget {
   Products([this.products = const []]) {
     print('[Products Widget] Constructor');
   }
+  
+  Widget _buidProductItem(BuildContext context , int index){
+    return Card(
+      elevation: 1.0,
+      child: Column(
+        children: <Widget>[
+          Image.network("https://cdn.pixabay.com/photo/2018/10/19/20/51/walnut-3759573__480.jpg",fit: BoxFit.cover,),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(products[index]),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProductLists(){
+    Widget productCard;
+    if(products.length >0){
+      productCard = ListView.builder(
+        itemBuilder:  _buidProductItem,
+        itemCount: products.length,
+      );
+    }
+/*    else{
+      productCard = Center(child: new Text("No Pproduct Found Please add Some"),);
+    }*/
+
+else{
+  productCard =Container();
+    }
+    return productCard;
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
-    print('[Products Widget] build()');
-    return ListView(
-      children: products
-          .map(
-            (element) => Card(
-                  child: Column(
-                    children: <Widget>[
-                      Image.network("https://cdn.pixabay.com/photo/2018/10/22/11/58/grass-3765172__480.jpg",fit: BoxFit.cover,),
-                      Text(element)
-                    ],
-                  ),
-                ),
-          )
-          .toList(),
-    );
+    return _buildProductLists();
   }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('EasyList'),
         ),
-        body: ProductManager(startingProduct: 'Food Tester'),
+        body: ProductManager(),
       ),
     );
   }
